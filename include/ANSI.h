eccbce69340d1b839e67559950d29c50d7518f80
Abu Bakar <111528426+abubakar132@users.noreply.github.com>
2023-12-23 21:41:46 +0500

    Create ANSI.h

---
+++ include/ANSI.h
@@ -0,0 +1,10 @@
+// ANSI.h
+#pragma once
+
+// ANSI escape codes for text color
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define CYAN    "\033[36m"
+
